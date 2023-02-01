67268706e9c0e07e4f2527c89051006d0d220e9c
Maverik Apollo Minett <maverik@maverik.io>
2023-02-01 13:27:27 -0500

    add http server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import * as http from 'http';
+
+const port = 3000;
+
+http.createServer( 
+    ( request, response ) => {
+        response.write('Hello world!')
+        response.end()
+    },
+)
+.listen(
+    port, () => console.log(`Server started at http://localhost:${port}`)
+)
