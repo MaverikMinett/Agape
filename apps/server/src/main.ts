1e3ffecba8c8e1981e3c6d5b995441882b5ee3f1
Maverik Apollo Minett <maverik@maverik.io>
2023-02-02 07:31:18 -0500

    server/ add proxy to angular application

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,13 +1,30 @@
 import * as http from 'http';
+import { proxy } from './proxy';
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+const verbose = true;
 
-http.createServer( 
-    ( request, response ) => {
-        response.write('Hello world!')
+const server = http.createServer()
+
+server.on('request', async ( request, response ) => {
+
+    /* logging */
+    if ( verbose ) console.log( request.url )
+
+    /* redirect to angular app */
+    let match: RegExpMatchArray = request.url.match(/^\/(?<path>.*)$/)
+    if ( match ) {
+        console.log(match.groups)
+        proxy('http://localhost:4200', match.groups.path, request, response )
+        return;
+    }
+    else {
+        response.write('404 Page not found')
         response.end()
-    },
-)
-.listen(
+    }
+
+} )
+
+server.listen(
     port, () => console.log(`Server started at http://localhost:${port}`)
 )
