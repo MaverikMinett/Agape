688f780fe9dcdfb9d924c0a4f309d86b4450b433
Maverik Apollo Minett <maverik@maverik.io>
2023-02-02 09:49:40 -0500

    add lib-server library

---
+++ libs/lib-server/README.md
@@ -0,0 +1,11 @@
+# lib-server
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build lib-server` to build the library.
+
+## Running unit tests
+
+Run `nx test lib-server` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/lib-server/src/lib/proxy.ts
@@ -0,0 +1,23 @@
+import http from 'http';
+import pathlib from 'path';
+
+import { URL } from 'url';
+
+export async function proxy( to: string, path: string, request: http.IncomingMessage, response: http.ServerResponse ) {
+    const { hostname, pathname, port } = new URL(to)
+
+    const options =  {
+        hostname,
+        port,
+        path: pathlib.join(pathname,path),
+        method: request.method,
+        headers: request.headers
+    };
+
+    const proxy = http.request( options, result => {
+        response.writeHead(result.statusCode as number, result.headers)
+        result.pipe( response, { end: true })
+    } )
+
+    request.pipe( proxy, { end: true } )
+}
