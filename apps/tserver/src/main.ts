1378c917b9c70d07e17edccb4a66fca82599b138
Maverik Apollo Minett <maverik@maverik.io>
2023-02-02 11:34:48 -0500

    use log middleware in express applications

--- apps/tserver/src/main.ts
+++ apps/tserver/src/main.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { proxy } from '@lib/express'
+import { log, proxy } from '@lib/express'
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3002;
 const verbose = true;
@@ -7,25 +7,13 @@ const verbose = true;
 const app = express();
 
 /* logging */
-if ( verbose ) {
-  app.use( (req, res, next ) => {
-    const { method, url } = req;
-    const { statusCode, statusMessage } = res;
-  
-    res.on('finish', () => {
-      console.log(`${statusCode} ${method} ${url}`)
-      if ( statusMessage ) console.log(`        ${statusMessage}`)
-    })
-  
-    next()
-  })
-}
+if ( verbose ) app.use( log )
 
+/* redirect to angular app */
 app.use(['/', '/*'], proxy('http://localhost:4200') )
 
+/* 404 */
 app.use('*', (req, res) => {
-  console.log("base", req.baseUrl)
-  console.log("url", req.url)
   res.send('404 Page not found')
 })
 
