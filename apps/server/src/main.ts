04a74a6609da98e373ffb0d979d3d7151dfaee55
Maverik Apollo Minett <maverik@maverik.io>
2023-04-07 21:41:30 -0400

    add api endpoint with swagger documentation to $server app

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,6 +1,7 @@
 import * as http from 'http';
 import { log, proxy } from '@lib/server';
-import * as fs from 'fs';
+import fs from 'fs';
+import { extname } from 'path';
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3001;
 const verbose = true;
@@ -22,28 +23,49 @@ server.on('request', async ( request, response ) => {
         return;
     }
 
-    /** redirects **/
+    /** routing **/
     let match: RegExpMatchArray
 
-    /* redirect to angular app */
-    match = request.url.match(/^\/ng(?<path>.*\/?)$/)
-    if ( match ) {
-        console.log(`Forwarding to angular frontend ${match.groups.path}`)
-        proxy('http://localhost:4200', match.groups.path ?? "", request, response )
+    try {
 
-        return;
+        /* display swagger documentation */
+        match = request.url.match(/^\/api\/?$/) || request.url.match(/^\/api\/(?<path>.*)$/)
+        if ( match ) {
+            const path = match.groups?.path ?? 'index.html'
+            const suffix = match.groups?.path ? extname(path) : '';
+
+            let content = fs.readFileSync(`./apps/_swagger/${path}`)
+            console.log(response.getHeaders())
+            response.write( content )
+            response.end()
+            return;
+        }
+
+        /* redirect to angular app */
+        match = request.url.match(/^\/ng$/) ?? request.url.match(/^\/ng\/(?<path>.*)$/)
+        if ( match ) {
+            console.log(`Forwarding to angular frontend ${match.groups?.path}`)
+            proxy('http://localhost:4200', match.groups?.path ?? "/", request, response )
+            return;
+        }
+
+        /* redirect to project zed frontend */
+        match = request.url.match(/^\/zed$/) ?? request.url.match(/^\/zed\/(?<path>.*)$/)
+        if ( match ) {
+            console.log(`Forwarding to project zed frontend http://localhost:4201${match.groups?.path}`)
+            proxy('http://localhost:4201', match.groups?.path ?? "", request, response )
+            return;
+        }
+
+        /* page not found error */
+        response.write('404 Page not found')
+        response.end()
+
+    } catch (error) {
+        console.log(error)
     }
 
-    match = request.url.match(/^\/zed(?<path>.*\/?)$/)
-    if ( match ) {
-        console.log(`Forwarding to agape frontend http://localhost:4201${match.groups.path}`)
-        proxy('http://localhost:4201', match.groups.path ?? "", request, response )
-        return;
-    }
 
-    /* page not found error */
-    response.write('404 Page not found')
-    response.end()
 } )
 
 server.listen(
