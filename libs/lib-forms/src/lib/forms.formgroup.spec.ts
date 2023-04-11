9d7a412b3cac30a72ad7d4e9ef8229b663b226e1
Maverik Minett <maverik@maverik.io>
2023-04-11 11:56:09 -0400

    add test for has to FormGroup in /lib-forms

--- libs/lib-forms/src/lib/forms.formgroup.spec.ts
+++ libs/lib-forms/src/lib/forms.formgroup.spec.ts
@@ -26,10 +26,18 @@ describe('FormGroup', () => {
             f = new FormGroup().string('name')  
             expect(f).toBeInstanceOf(FormGroup)
         })
+        it('should have the string fields', () => {
+            f = new FormGroup().string('name')  
+            expect(f.has('name')).toBe(true)
+        })
     })
 
     describe('has', () => {
-
+        it('should have the field', () => {
+            f = new FormGroup()
+            f.string('name')   
+            expect(f.has('name')).toBeTruthy()
+        })
     })
 
 });
