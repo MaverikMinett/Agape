c94eb30229ee514b9c21b4cc996ccf3595ef9906
Maverik Minett <maverik@maverik.io>
2023-04-13 13:19:27 -0400

    documentation for keypress /lib-cli

--- libs/lib-cli/src/lib/keypress.ts
+++ libs/lib-cli/src/lib/keypress.ts
@@ -14,9 +14,21 @@ interface KeypressEvent {
 
 
 // let listeningToKeypresses = false;
+// TODO: getting a warning about the number is listeneres on stdin and stdout
+// which is unexpected because I am using both `removeListener` and `stdin.once`
+// both of which should remove the listenere after it is set (only one of these
+// should be necessary...) but the warning persists. Can rewrite this to only
+// listen once and then allow subscriptions to that listener (toggle it on and 
+// off between keypresses)
 process.stdin.setMaxListeners(0)
 process.stdout.setMaxListeners(0)
-export function keypress() {
+
+
+/**
+ * Wait for a keypress
+ * @returns Promise
+ */
+export async function keypress() {
     process.stdin.setRawMode(true); 
 
     const promise = new Promise<KeypressEvent>( (resolve, reject) => {
