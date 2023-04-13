e62654536da9cd2a76c11a298c33521cb599d2e9
Maverik Minett <maverik@maverik.io>
2023-04-13 19:34:54 -0400

    getCursorPosition returns numbers /lib-cli

--- libs/lib-cli/src/lib/cursor.ts
+++ libs/lib-cli/src/lib/cursor.ts
@@ -1,24 +1,26 @@
 
 
-
+export interface CursorPosition {
+    row: number;
+    col: number;
+}
 
 /**
  * Get the cursor position in the terminal
  * @returns Promise
  */
-export async function getCursorPosition(): Promise<{rows: number, cols: number}> {
+export async function getCursorPosition(): Promise<CursorPosition> {
     const termCodeGetCursorPosition =  '\u001b[6n'
 
-    const promise = new Promise<{rows: number, cols: number}>( (resolve) => {
+    const promise = new Promise<{row: number, col: number}>( (resolve) => {
         process.stdin.setEncoding('utf8')
         process.stdin.setRawMode(true)
             const findCursor = function() {
-                let position: { rows: number, cols: number }
                 const buffer = process.stdin.read();
                 const string = JSON.stringify(buffer); // "\u001b[9;1R"
                 const regex = /\[(.*)/g;
                 const xy = regex.exec(string)[0].replace(/\[|R"/g, '').split(';');
-                position = { rows: Number(xy[0]), cols: Number(xy[1]) };
+                const position:CursorPosition = { row: Number(xy[0]) - 1, col: Number(xy[1]) - 1 };
                 resolve(position)
             }
 
