e7be006d4dfabbb8ec2d8a3b9a8dc796358c521a
Maverik Minett <maverik@maverik.io>
2023-04-21 10:50:13 -0400

    return _id as id in mongo queries $clientng

--- apps/tserver/src/app/events/events.repo.ts
+++ apps/tserver/src/app/events/events.repo.ts
@@ -1,19 +1,40 @@
 
 import db from '../../db'
 import { ObjectId } from 'mongodb'
-import { stringifyId } from '../../shared/db-helper'
-
-
 
 export async function listEvents() {
-    const events = await db().collection('events').find().toArray()
-    events.map( stringifyId )
+    const events = await db()
+        .collection('events')
+        .find(
+            {},
+            { 
+                projection: {
+                    _id: 0,
+                    id: {$toString: "$_id" },
+                    name: 1,
+                }
+            }
+        )
+        .toArray()
+
     return events
 }
 
 export async function retrieveEvent( id: string ) {
     const _id = new ObjectId(id)
-    const event = await db().collection('events').findOne({ _id })
-    stringifyId(event)
+
+    const event = await db()
+        .collection('events')
+        .findOne(
+            { _id },
+            { 
+                projection: {
+                    _id: 0,
+                    id: {$toString: "$_id" },
+                    name: 1,
+                }
+            }
+        )
+
     return event
 }
