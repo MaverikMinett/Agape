74a627989e7674cfefb04fbb4d2d87be68ce70d7
Maverik Minett <maverik@maverik.io>
2023-04-21 18:39:13 -0400

    add joi validation $tserver

--- apps/tserver/src/app/events/events.routes.ts
+++ apps/tserver/src/app/events/events.routes.ts
@@ -4,8 +4,9 @@ import { getEvent, getEvents, modifyEvent, newEvent, removeEvent } from './event
 import { IEvent } from './event.interface'
 import { OptionalId } from '../../types';
 
-export const eventsRouter = Router()
+import Joi  from 'joi'
 
+export const eventsRouter = Router()
 
 
 eventsRouter.get( '/events', async ( request, response ) => {
@@ -13,9 +14,16 @@ eventsRouter.get( '/events', async ( request, response ) => {
     response.send(events)
 })
 
+const createEventValidationSchema = Joi.object({
+    name: Joi.string().required()
+})
 
 eventsRouter.post( '/events', async ( request, response ) => {
     const payload: OptionalId<IEvent,string> = request.body
+
+    const { error } = createEventValidationSchema.validate(payload)
+    if ( error ) throw error
+
     const result = await newEvent({payload})
     response.status(201).send(result)
 })
@@ -27,21 +35,26 @@ eventsRouter.delete( '/events/:id', async ( request, response ) => {
     response.status(204).send(undefined)
 })
 
+const updateEventValidationSchema = Joi.object({
+    name: Joi.string().required()
+})
 
 eventsRouter.put( '/events/:id', async ( request, response ) => {
     const id = request.params.id
     const payload: IEvent = request.body
+
+
+    const { error } = updateEventValidationSchema.validate(payload)
+    if ( error ) throw error
+
     await modifyEvent({id, payload})
     response.send(undefined)
 })
-
 
 eventsRouter.get( '/events/:id', async ( request, response ) => {
     const id = request.params.id
     const events = await getEvent({id})
     response.send(events)
 })
-
-
 
 export default eventsRouter
