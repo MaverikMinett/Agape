67be3031a32ddcad411aa04ffd78cf3054ca14fe
Maverik Minett <maverik@maverik.io>
2023-04-22 07:50:55 -0400

    added environment files and environment service to client-ng

--- apps/clientng/src/app/app.module.ts
+++ apps/clientng/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { EventsModule } from './modules/events/events.module';
+import { EnvironmentModule } from './shared/environment/environment.module';
+
+import env from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    EnvironmentModule.forRoot(env),
     EventsModule,
   ],
   providers: [],
---
+++ apps/clientng/src/environments/environment.ts
@@ -0,0 +1,9 @@
+
+
+export const env = {
+    "apis": {
+        "tserver": "http://localhost:3002/api"
+    }
+}
+
+export default env;
