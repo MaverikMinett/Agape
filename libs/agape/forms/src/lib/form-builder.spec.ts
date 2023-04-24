57392cbcb427e02d17927c023cd207292c6a1fe1
Maverik Minett <maverik@maverik.io>
2023-04-24 07:09:07 -0400

    add tests @agape/forms

--- libs/agape/forms/src/lib/form-builder.spec.ts
+++ libs/agape/forms/src/lib/form-builder.spec.ts
@@ -30,5 +30,18 @@ describe('FormBuilder', () => {
         })
     })
 
+    describe('number', () => {
+        beforeEach( () => {
+            fb = new FormBuilder()
+        })
+        it('should return a new FormGroup instance', () => {
+            expect( fb.number('test') ).toBeInstanceOf(FormGroup)
+        })
+        it ('should have called string on the new formgroup instance', () => {
+            const f = fb.number('test')
 
+            expect( f.has('test') ).toBeTruthy()
+            expect( f.get('test').type ).toBe('number')
+        })
+    })
 })
