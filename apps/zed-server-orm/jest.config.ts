73aea0d445022b1837317b744e6d51a6ce513f43
Maverik Minett <maverik@maverik.io>
2023-04-26 07:50:10 -0400

    $zed-server-orm initial commit

---
+++ apps/zed-server-orm/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'zed-server-orm',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/zed-server-orm',
+};
