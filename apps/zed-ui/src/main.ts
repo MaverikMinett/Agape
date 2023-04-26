f778d101613aac659749c86defa231b18388366d
Maverik Minett <maverik@maverik.io>
2023-04-26 14:34:53 -0400

    zed-ui initial commit

--- apps/zed-ui/src/main.ts
+++ apps/zed-ui/src/main.ts
@@ -1,1 +1,9 @@
-import './app/app.element.ts';
+// import './app/app.element.ts';
+
+import 'reflect-metadata';
+
+import { FooComponent } from './app/foo/foo.component';
+import { bootstrapComponent } from './lib/core';
+
+const appRoot: HTMLElement = document.getElementById('app-root');
+bootstrapComponent(FooComponent, appRoot);
