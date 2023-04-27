9d4d4187946d37d9f76c7eee5ed6981465694c06
Maverik Minett <maverik@maverik.io>
2023-04-27 10:07:53 -0400

    zed-ui routing app

--- apps/zed-ui-routing/src/main.ts
+++ apps/zed-ui-routing/src/main.ts
@@ -1,1 +1,9 @@
-import './app/app.element.ts';
+// import './app/app.element.ts';
+
+import 'reflect-metadata';
+
+import { FooComponent } from './app/foo.component';
+import { bootstrapComponent } from './lib/index';
+
+const appRoot: HTMLElement = document.getElementById('app-root');
+bootstrapComponent(appRoot, FooComponent);
