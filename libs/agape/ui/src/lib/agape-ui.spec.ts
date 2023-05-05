e9159665b22bec66abe047bf98edb03a21a684b8
Maverik Minett <maverik@maverik.io>
2023-05-05 18:20:14 -0400

    remove dead @agape/ui lib

--- libs/agape/ui/src/lib/agape-ui.spec.ts
+++
@@ -1,7 +0,0 @@
-import { agapeUi } from './agape-ui';
-
-describe('agapeUi', () => {
-  it('should work', () => {
-    expect(agapeUi()).toEqual('agape-ui');
-  });
-});
--- libs/agape/ui/src/lib/agape-ui.ts
+++
@@ -1,3 +0,0 @@
-export function agapeUi(): string {
-  return 'agape-ui';
-}
