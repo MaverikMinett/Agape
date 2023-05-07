70327f7bfeb933dc02d6e1e8c410f678001b8f08
Maverik Minett <maverik@maverik.io>
2023-05-07 15:54:32 -0400

    updated index page ov ng-tied-subscriptions

--- apps/ng-tied-subscriptions/src/app/index-page/index-page.component.ts
+++ apps/ng-tied-subscriptions/src/app/index-page/index-page.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'project-zed-index-page',
-  templateUrl: './index-page.component.html'
+  templateUrl: './index-page.component.html',
+  styleUrls: ['./index-page.component.scss'],
 })
 export class IndexPageComponent {
 
