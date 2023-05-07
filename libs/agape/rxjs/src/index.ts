cfce83b4df9f44e133b8e572ded6b66d87fad13f
Maverik Minett <maverik@maverik.io>
2023-05-07 06:12:31 -0400

    import @agape/rxjs into repo

--- libs/agape/rxjs/src/index.ts
+++ libs/agape/rxjs/src/index.ts
@@ -1,1 +1,3 @@
-export * from './lib/agape-rxjs';
+export * from './lib/tie';
+
+
--- libs/agape/rxjs/src/lib/agape-rxjs.spec.ts
+++
@@ -1,7 +0,0 @@
-import { agapeRxjs } from './agape-rxjs';
-
-describe('agapeRxjs', () => {
-  it('should work', () => {
-    expect(agapeRxjs()).toEqual('agape-rxjs');
-  });
-});
