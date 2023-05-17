885a2a3f66c062f8e690246fabe0b7b0cfe7c6e5
Maverik Minett <maverik@maverik.io>
2023-05-17 15:20:15 -0400

    add auth module to nest-server-orm

---
+++ apps/nest-server-orm/src/app/api/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import bcrypt from 'bcryptjs';
+
+import { orm } from '@agape/orm';
+import { Interface } from '@agape/types';
+import { Exception } from '@agape/exception';
+
+import { Credentials, User } from 'lib-platform';
+
+
+@Injectable()
+export class AuthService {
+
+    constructor( private jwtService: JwtService ) {
+
+    }
+    
+    async login( credentials: Interface<Credentials> ) {
+        
+        const user = await orm.lookup( User, { username: credentials.username } ).exec()
+
+        const authenticated = bcrypt.compareSync(credentials.password, user.password)
+
+        if ( ! authenticated ) {
+            throw new Exception(401)
+        }
+
+        const payload = { username: user.username, sub: user.id };
+
+        const response =  {
+          token: await this.jwtService.signAsync(payload),
+        };
+
+        return response
+    }
+
+
+}
