eb369883f90f298cdb5b003d7b49f49f41ea5044
Maverik Minett <maverik@maverik.io>
2023-08-12 08:55:30 -0400

    move retrieve query into orm file

--- libs/agape/orm/src/lib/orm.ts
+++ libs/agape/orm/src/lib/orm.ts
@@ -1,13 +1,15 @@
+import { Document, Model, ViewDescriptor } from '@agape/model';
+import { inflate } from '@agape/object';
+import { pluralize, camelize } from '@agape/string'
 import { Class, Dictionary } from '@agape/types'
-import { Collection } from 'mongodb';
+import { Collection, ObjectId } from 'mongodb';
+
 import { MongoDatabase } from './databases/mongo.database';
-import { RetrieveQuery } from './mongo/queries/retrieve.query';
+
 import { ListQuery } from './mongo/queries/list.query';
 import { DeleteQuery } from './mongo/queries/delete.query';
 import { UpdateQuery } from './mongo/queries/update.query';
-import { pluralize, camelize } from '@agape/string'
 import { InsertQuery } from './mongo/queries/insert.query';
-import { Model, ViewDescriptor } from '@agape/model';
 import { LookupQuery } from './mongo/queries/lookup.query';
 
 
@@ -172,4 +174,69 @@ export class Orm {
     }
 
 
+}
+
+
+export class RetrieveQuery<T extends Class> {
+
+    constructor( public orm: Orm, public model: T, public collection: Collection, public id: string ) {
+
+    }
+
+    async exec( ): Promise<Pick<InstanceType<T>, keyof InstanceType<T>>> {
+        let _id: ObjectId
+        try {
+            _id = new ObjectId(this.id)
+        }
+        catch {
+            throw new Error(`Invalid record ${this.id}`)
+        }
+
+        const descriptor = Model.descriptor(this.model)
+
+        /* projection */
+        const projection: Dictionary = { _id: 0 }
+
+        const primaryField = descriptor.fields.all().find( f => f.primary )
+        if ( primaryField ) {
+            projection[primaryField.name] = { $toString: "$_id" }
+        }
+
+        const otherFields = descriptor.fields.all().filter( f => ! f.primary )
+        for ( let field of otherFields ) {
+            projection[field.name] = 1
+        }
+
+        /* selection */
+        const selection: Dictionary = { _id }
+        
+        /* mongo query */
+        const record = await this.collection.findOne( selection, { projection } )
+
+        /* record not found */
+        if ( ! record ) return undefined
+
+        const item = {}
+        item[primaryField.name] = record[primaryField.name]
+
+        for ( let field of otherFields ) {
+            if ( field.designType instanceof Function && field.designType.prototype as any instanceof Document ) {
+                const objectId: ObjectId = record[field.name]
+                const idString = objectId.toString()
+                item[field.name] = await this.orm.retrieve(field.designType, idString).exec()
+                console.log(objectId, idString, item[field.name])
+            }
+            else {
+                item[field.name] = record[field.name]
+            }
+        }
+
+        /* record */
+        return item as any
+    }
+
+    async inflate( ): Promise<InstanceType<T>> {
+        const record = await this.exec()
+        return inflate<T>( this.model, record )
+    }
 }
